01991deffd125b34ffe9d7007589239f972fe30a
Yusuke Kawasaki <u-suke@kawa.net>
2022-11-27 21:06:53 +0900

    npm run test-timezone

--- src/add.ts
+++ src/add.ts
@@ -5,13 +5,13 @@ const enum d {
     DAY = 24 * HOUR,
 }
 
-const addMonth = (dt: Date, diff: number): Date => {
+const addMonth = (dt: Date, months: number): Date => {
     dt = new Date(+dt);
     const year = dt.getFullYear();
     const month = dt.getMonth();
     const date = dt.getDate();
     dt.setDate(1);
-    let newMonth = year * 12 + month + diff;
+    let newMonth = year * 12 + month + months;
     dt.setFullYear(Math.trunc(newMonth / 12));
     newMonth %= 12;
     dt.setMonth(newMonth);
@@ -20,6 +20,20 @@ const addMonth = (dt: Date, diff: number): Date => {
         dt.setDate(0); // the very last day of the previous month
     }
     return dt;
+};
+
+const addDay = (dt: Date, days: number): Date => {
+    const tz1 = dt.getTimezoneOffset();
+    dt = new Date(+dt + days * d.DAY);
+    const tz2 = dt.getTimezoneOffset();
+    if (tz1 !== tz2) {
+        dt = new Date(+dt + (tz2 - tz1) * d.MINUTE);
+    }
+    return dt;
+};
+
+const addMillisecond = (dt: Date, milliseconds: number): Date => {
+    return new Date(+dt + milliseconds);
 };
 
 export const add = (dt: Date, diff: number, unit: string): Date => {
@@ -31,28 +45,22 @@ export const add = (dt: Date, diff: number, unit: string): Date => {
             return addMonth(dt, diff);
 
         case "week":
-            diff *= d.DAY * 7;
-            break;
+            return addDay(dt, diff * 7);
 
         case "day":
-            diff *= d.DAY;
-            break;
+            return addDay(dt, diff);
 
         case "hour":
-            diff *= d.HOUR;
-            break;
+            return addMillisecond(dt, diff * d.HOUR);
 
         case "minute":
-            diff *= d.MINUTE;
-            break;
+            return addMillisecond(dt, diff * d.MINUTE);
 
         case "second":
-            diff *= d.SECOND;
-            break;
-    }
+            return addMillisecond(dt, diff * d.SECOND);
 
-    if (diff) {
-        dt = new Date(+dt + diff);
+        case "millisecond":
+            return addMillisecond(dt, diff);
     }
 
     return dt;
