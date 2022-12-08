42c7259ec9a20a2050f5fbea7d0ab3a4df546869
Yusuke Kawasaki <u-suke@kawa.net>
2022-12-08 13:11:21 +0900

    refactor: ($cdate, $strftime)

--- browser/wrap.cjs
+++ browser/wrap.cjs
@@ -2,9 +2,9 @@
 var cdate, strftime; // PRE
 !((__then) => { // PRE
 
-    __then(X_cdate, X_strftime);
-})((X_cdate, X_strftime) => {
+    __then($cdate, $strftime);
+})(($cdate, $strftime) => {
     const __exports = ("undefined" !== typeof exports ? exports : {});
-    cdate = __exports.cdate = X_cdate;
-    strftime = __exports.strftime = X_strftime;
+    cdate = __exports.cdate = $cdate;
+    strftime = __exports.strftime = $strftime;
 });
