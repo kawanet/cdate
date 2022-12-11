3a922965cfb4f24de10af4a829856da0fe4751f5
Yusuke Kawasaki <u-suke@kawa.net>
2022-12-12 01:42:15 +0900

    refactor: module import

--- index.ts
+++ index.ts
@@ -1,6 +1,2 @@
-import type * as types from "./types/cdate";
-import {cdate as cdateFn} from "./src/cdate.js";
-import {strftime as strftimeFn} from "./src/format/texter.js";
-
-export const cdate: typeof types.cdate = cdateFn;
-export const strftime: typeof types.strftime = strftimeFn;
+export {cdate} from "./src/cdate.js";
+export {strftime} from "./src/format/texter.js";
